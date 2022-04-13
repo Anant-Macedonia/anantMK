f29dd52dcbe1203b5616e3a2047403efcd22c276
kire21 <kire.zdraveski@gmail.com>
2022-04-13 10:07:18 +0200

    basic setup

---
+++ components/Navigation.js
@@ -0,0 +1,10 @@
+const Navigation = () => {
+  return (
+    <div>
+      <h1>Navigation</h1>
+      <a href="/blog">Blog</a>
+    </div>
+  );
+};
+
+export default Navigation;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { ApolloProvider } from "@apollo/client";
+import { client } from "../lib/apollo";
+import Layout from "../components/Layout";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
