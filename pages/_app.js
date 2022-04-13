f7b5025311a438eddbf9a8b06645131c9ce70d64
Petar <72278293+pepi05@users.noreply.github.com>
2022-04-13 14:33:57 +0200

    added carousel

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { ApolloProvider } from "@apollo/client";
 import Layout from "../components/Layout/Layout";
 import { client } from "../lib/apollo";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "../styles/globals.css";
 
