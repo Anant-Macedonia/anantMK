e721fc294b1a4d36ea0e2eee546a48219b2387bf
Kire Zdraveski <kire.zdraveski@gmail.com>
2022-08-18 09:27:29 +0200

    removed console log

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,9 +1,7 @@
 import { Box, Button, Container, Typography } from "@mui/material";
-import Image from "next/image";
 import classes from "./hero.module.css";
 
 const Hero = ({ heroData }) => {
-  console.log(heroData.image.sourceUrl);
   return (
     <>
       <Box
