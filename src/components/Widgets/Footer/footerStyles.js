5fbe28afa0cc8530b9565186ceed9b3051f496a0
Petar Gjuzelov <petargjuzelov@yahoo.com>
2022-09-22 15:13:35 +0200

    implemented footer

---
+++ src/components/Widgets/Footer/footerStyles.js
@@ -0,0 +1,14 @@
+export const linkText = {
+  fontSize: "20px",
+  color: "#FFFFFF",
+  fontWeight: "400",
+  textDecoration: "underline",
+  letterSpacing: "-0.02em",
+  lineHeight: "34.7px",
+};
+
+export const contactText = {
+  fontSize: "22px",
+  letterSpacing: "-0.125em",
+  lineHeight: "44px",
+};
